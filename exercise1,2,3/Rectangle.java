3648fc046857e1d72aed79fcc805afb0c1f335f1
GiorgiAbzianidzeSE <84072183+GiorgiAbzianidzeSE@users.noreply.github.com>
2022-05-27 22:16:01 +0400

    Add files via upload

---
+++ exercise1,2,3/Rectangle.java
@@ -0,0 +1,38 @@
+//პაკეტი:
+package ge.ufc.exercises;
+
+public class Rectangle {
+
+    private float height;
+    private float weight;
+
+    public void setHeight(float height){
+        this.height = height;
+    }
+
+    public float getHeight(){
+        return this.height;
+    }
+
+    public void setWeight(float weight){
+        this.weight = weight;
+    }
+
+    public float getWeight(){
+        return this.weight;
+    }
+
+    //final ვინაიდან ამ მეთოდის იერარქიულობა შეიზღუდოს და მხოლოდ პაკეტში ჩანდეს( კარგი ხერხია რომ ჩავანაცვლოთ default წვდომის მოდიფიკატორი)
+    protected final double perimeterCalculator(){
+        return (this.weight + this.height) * 2;
+    }
+
+    protected final double areaCalculator(){
+        return this.height * this.weight;
+    }
+
+    public int compareTo(Rectangle rectangle){
+        if(rectangle.areaCalculator() == this.areaCalculator()) return 0;
+        return rectangle.areaCalculator() > this.areaCalculator() ? 1 : -1;
+    }
+}
